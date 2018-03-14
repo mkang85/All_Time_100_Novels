2ff3abae711e157e2860083171d606e66b1d8da2
Michael Kang <michaelkang85@gmail.com>
2018-03-13 23:00:43 -0400

    starting the pseudo code process

--- lib/All_Time_100_Novels/CLI.rb
+++ lib/All_Time_100_Novels/CLI.rb
@@ -2,7 +2,16 @@
 class AllTime100Novels::CLI
 
   def call
-    puts "Welcome to Time Magazine's 100 Best English Language Novels"
+    list_titles
+    menu
   end
 
+  def list_titles
+    puts "Welcome to Time Magazine's 100 Best English Language Novels"
+    puts "All the titles"
+  end
+
+  def menu
+    puts "Would you like to see the whole list or go by letter?"
+  end
 end
