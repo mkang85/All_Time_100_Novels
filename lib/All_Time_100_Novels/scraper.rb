c724fbf9966e3e3a787712ab6852cc66c8bc4f20
Michael Kang <michaelkang85@gmail.com>
2018-03-18 14:32:03 -0400

    got the url, got the position...need the title

--- lib/All_Time_100_Novels/scraper.rb
+++ lib/All_Time_100_Novels/scraper.rb
@@ -1,8 +1,16 @@
 class AllTime100Novels::Scraper
   def get_page
+    title_info = []
     doc = Nokogiri::HTML(open("http://entertainment.time.com/2005/10/16/all-time-100-novels/slide/all/"))
+    titles = doc.css(".items a")
+    titles_a = doc.css("i")
     binding.pry
+    titles.for each do |title|
+      position = title.attr("data-pos").value
+      url = title.attr("href").value
+    end
   end
+
 
 
 end
